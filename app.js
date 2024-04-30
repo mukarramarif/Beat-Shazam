20bdeecc407099934128100e6f2b1a2be80608f9
Matt Schoonmaker <mls621@lehigh.edu>
2024-04-30 18:07:10 -0400

    Fix

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const Database =require( "better-sqlite3");
 const app = express();
-app.use(express.json());
+app.use(express.static(
+    path.resolve(__dirname, "public")
+  ));
 
 app.listen(3000, () => console.log("Go Beat Shazam!"));
