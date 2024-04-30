c34ca67253d3a46dfdd1f7596b9df8481787740d
Miles Jaffee <mej327@lehigh.edu>
2024-04-30 18:03:10 -0400

    removed better-sqlite

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const Database =require( "better-sqlite3");
+//const Database =require( "better-sqlite3");
 const app = express();
 app.use(express.json());
 
