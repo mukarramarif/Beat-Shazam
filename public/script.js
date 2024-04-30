4b37b58cba10d4e9b9d6dedc505349bf86bef4d4
Miles Jaffee <mej327@lehigh.edu>
2024-04-30 18:44:51 -0400

    set up playlist a bit

--- public/script.js
+++ public/script.js
@@ -8,8 +8,14 @@ function init() {
         e.preventDefault();
         console.log(" submit button clicked");
 
+        
+        //TODO get song from given playlist
 
-}
+        $("#container").clear();
+
+        //TODO construct guessing page
+
+    }
     );
         
 }
