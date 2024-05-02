c7f9c34437d6890e444b55187520c0d73e1819ff
Muhammad Mukarram Arif <mukarramarif03@gmail.com>
2024-05-02 12:25:33 -0400

    fixed merging

--- public/script.js
+++ public/script.js
@@ -7,74 +7,6 @@ $(document).ready(function() {
         let playlistId = pathSegments[2];
         
         console.log(playlistId);
-
-        $.ajax({
-            url: "/send-playlist",
-            type: 'GET',
-            data: { id: playlistId },
-            success: function(data) { //successfully sent the playlist link
-                console.log(data);
-                song = createSongElement(data);
-                $("#container").hide();
-                
-                $("#part-2-container").show();
-                $("#songFile").attr("src", song.track.preview_url);
-                $("audio")[0].load();
-
-                $("#guess").attr("placeholder", "Guess the ARTIST...");
-                
-            }
-        });
-    });
-    $('#guessButton').click(function(){
-        let guess = $('#guess').val();
-        console.log(guess);
-        let foundArtist = false;
-        for(let i=0; i<song.track.artists.length; i++){
-            
-                console.log(song.track.artists[i]);
-                if(guess === song.track.artists[i].name){
-                    foundArtist = true;
-                    console.log("correct");
-
-                    beginTitleRound();
-                }
-        }
-        if (!foundArtist) {
-            console.log("Wrong");
-            
-        }
-        
-        
-    });
-});
-
-function createSongElement(songs){
-    let randomIndex = Math.floor(Math.random() * songs.items.length);
-    console.log(randomIndex);
-    let song = songs.items[randomIndex];
-    let songUrl= song.track.preview_url;
-    if(songUrl == null){
-        return createSongElement(songs);
-    }
-    console.log(songUrl);
-    return song;   
-}
-
-function beginTitleRound() {
-
-}
-
-
-$(document).ready(function() {
-    let song;
-    $('#submitButton').click(function() {
-        console.log($('#playlist-url-box').val());
-        let url = new URL($('#playlist-url-box').val());
-        let pathSegments = url.pathname.split('/');
-        let playlistId = pathSegments[2];
-        
-        console.log(playlistId);
         $.ajax({
             url: "/send-playlist",
             type: 'GET',
@@ -93,7 +25,7 @@ $(document).ready(function() {
                 $("#thumbnail").attr("src", getThumbnail(song));
                 $("audio")[0].load();
             
-                
+                $("#guess").attr("placeholder", "Guess the ARTIST...");
                 
 
             }
